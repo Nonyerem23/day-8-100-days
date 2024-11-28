be6209d3d8b28ceadeef86525d0047056d286bd5
Nonyerem23 <mc.chinonye@stu.unizik.edu.ng>
2024-11-28 20:35:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("CUSTOM INSULT GENERATOR")
+print("\033[35m Mean Old Insult Machine!\033[0m")
+print()
+name = input("Who are you?:")
+if name == "David" or name == "david":
+    print("Hi David")
+    hair = input("How much hair do you have on your head?")
+    if hair =="None" or hair == "none":
+        print("Your head is bald") 
+    else:
+        print("Chai", name ,"you have a bald hair, I'm sorry about that.")
+    scale = input("On a scale of 1 - 10, how bald are you?")
+    if scale == "1" or scale == "2" or scale == "3" or scale == "4" or scale == "5":
+        print("""Hey David, keep your head bald, it's not gonna look good on you. You thought I wouldn't know who you were because you didn't capitalise properly!
+Well get that egg-head and dry your eyes because some amazing insults are coming, LOL!""")
+elif name == "Nonye" or name == "nonye":
+  print("Aww, you're obviouly a girl and can't be bald")
+else:
+      print("You'll have nice then")
